348b23d0133441f88632b0e3c28fc810dba52906
saheenshoukath <saheenshoukath@gmail.com>
2022-06-10 23:39:47 +0530

    add not found view for product

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="pt-16 p-2 h-screen">
-    <div v-if="!product && !loading">
-      <p class="text-center">Product not found.</p>
-    </div>
-    <div
-      class="sm:mt-10 flex flex-col sm:flex-row w-full justify-center h-2/3"
-      v-if="product"
+  <div v-if="!product && !loading">
+    <main
+      class="h-screen w-full flex flex-col justify-center items-center bg-red-200"
     >
+      <h1 class="text-9xl font-extrabold text-black tracking-widest">404</h1>
+      <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+        Product Not Found
+      </div>
+      <button class="mt-5">
+        <a
+          class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+        >
+          <span
+            class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+          ></span>
+
+          <span
+            class="relative block px-8 py-3 bg-[#1A2238] border border-current"
+          >
+            <router-link to="/">Go Home</router-link>
+          </span>
+        </a>
+      </button>
+    </main>
+  </div>
+
+  <div class="pt-16 p-2 h-screen" v-if="product">
+    <div class="sm:mt-10 flex flex-col sm:flex-row w-full justify-center h-2/3">
       <div class="w-full sm:w-1/2 lg:w-1/4">
         <img
           :src="product.image"

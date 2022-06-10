009c1ef2bd6439fbf9830ec8e24869c2d402c921
saheenshoukath <saheenshoukath@gmail.com>
2022-06-11 00:09:38 +0530

    add padding to navbar icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     class="p-2 sm:px-10 sm:py-2 justify-between flex items-center fixed w-full bg-white shadow z-20"
   >
     <router-link to="/" class="font-logo text-4xl">Brandly</router-link>
-    <div class="flex space-x-5">
+    <div class="flex space-x-5 mt-2">
       <div v-if="!isAuthenticated">
         <router-link to="/login"><p class="text-lg">Log in</p></router-link>
       </div>

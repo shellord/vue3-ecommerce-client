efee76328c4095c5ebe4d4313fd394e6cf06be48
saheenshoukath <saheenshoukath@gmail.com>
2022-06-10 23:35:46 +0530

    custom not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="pt-20 flex flex-col items-center justify-center w-full space-y-3">
-    <p class="text-xl">Page Not Found</p>
-    <p>
-      <router-link to="/"
-        ><a class="text-blue-500 underline">Go Home</a></router-link
+  <main
+    class="h-screen w-full flex flex-col justify-center items-center bg-red-200"
+  >
+    <h1 class="text-9xl font-extrabold text-black tracking-widest">404</h1>
+    <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+      Page Not Found
+    </div>
+    <button class="mt-5">
+      <a
+        class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
       >
-    </p>
-  </div>
+        <span
+          class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+        ></span>
+
+        <span
+          class="relative block px-8 py-3 bg-[#1A2238] border border-current"
+        >
+          <router-link to="/">Go Home</router-link>
+        </span>
+      </a>
+    </button>
+  </main>
 </template>
